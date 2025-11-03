56f82a0839e0f6ec6b333408cd39a7dddac6454a
Hemen Lahkar <hemenlahkar3108+github@gmail.com>
2025-11-03 14:49:34 +0530

    add insert last function

--- practical/34_doubly_LL.c
+++ practical/34_doubly_LL.c
@@ -57,12 +57,29 @@ int insert_first(Node **head, int x)
     return 1;
 }
 
+int insert_last(Node **head, int x)
+{
+    Node *temp = createNode(x);
+    if (temp == NULL) return 0;
+    if (*head == NULL)
+    {
+        *head = temp;
+        return 1;
+    }
+    Node *location = *head;
+    while(location->next != NULL) location = location->next;
+    location->next = temp;
+    temp->prev = location;
+    return 1;
+}
+
 int main()
 {
     Node *head = NULL;
     for(int i = 1; i < 6; i++)
         insert_first(&head, i+2);
-
+    for(int i = 1; i < 6; i++)
+        insert_last(&head, i * i + 3);
     printList(head);
     printListReverse(head);
     return 0;
